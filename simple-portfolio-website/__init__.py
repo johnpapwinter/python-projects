423da1ba8f836315ddb3447e6f6942b5f5addab2
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2023-03-24 22:48:17 +0200

    contact form

--- simple-portfolio-website/__init__.py
+++ simple-portfolio-website/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -21,6 +21,14 @@ def work():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    email = request.form['emailHead']
+    message = request.form['msgBody']
+    print(f'You have a message from {email}. It reads {message}')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
