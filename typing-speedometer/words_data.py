b196550b22be7e0718e41cc5c5daf75dad20348b
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2023-04-03 16:19:37 +0300

    returned list is not shuffled

--- typing-speedometer/words_data.py
+++ typing-speedometer/words_data.py
@@ -1,9 +1,12 @@
 import csv
+import random
 
 
 def fetch_words():
     with open('words.csv', newline='') as file:
         data = [row[0] for row in csv.reader(file)]
+
+    random.shuffle(data)
 
     return data
 
