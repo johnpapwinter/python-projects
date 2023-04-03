dcec4176171127158bdd30099e5ab1f6ee9f6163
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2023-04-03 20:51:52 +0300

    service class

---
+++ typing-speedometer/typometer_service.py
@@ -0,0 +1,16 @@
+import random
+from words_data import fetch_words
+
+
+class TypoSpeedometerService:
+    def __init__(self):
+        pass
+
+    def compare_words(self):
+        pass
+
+    def calculate_wpm(self):
+        pass
+
+    def reset(self):
+        pass
