86e1abc5eae116797e81bb99812d19fafccdf92a
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2023-04-03 21:16:43 +0300

    service class implementation

--- typing-speedometer/typometer_service.py
+++ typing-speedometer/typometer_service.py
@@ -4,13 +4,23 @@ from words_data import fetch_words
 
 class TypoSpeedometerService:
     def __init__(self):
-        pass
+        self.wordlist = fetch_words()
+        self.correct_words = 0
 
-    def compare_words(self):
-        pass
+    def compare_words(self, requested_word: str, given_word: str):
+
+        if requested_word.strip() == given_word.strip():
+            self.correct_words += 1
+            return True
+        else:
+            return False
 
     def calculate_wpm(self):
-        pass
+        return round(self.correct_words)
 
     def reset(self):
-        pass
+        self.wordlist = fetch_words()
+        self.correct_words = 0
+
+    def fetch_random_word(self):
+        return self.wordlist[random.randrange(len(self.wordlist))]
