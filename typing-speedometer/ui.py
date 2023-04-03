79a6a2998a229b92d51a47d2a02cfc2630c76209
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2023-04-03 15:32:27 +0300

    changed ui from tkinter to ctk

--- typing-speedometer/ui.py
+++ typing-speedometer/ui.py
@@ -1,19 +1,19 @@
-import tkinter as tk
+import customtkinter as tk
 
 
-class TypoSpeedometerUI(tk.Tk):
+class TypoSpeedometerUI(tk.CTk):
     def __init__(self):
-        tk.Tk.__init__(self)
+        tk.CTk.__init__(self)
         self.title("Typing Speedometer")
         self.geometry("500x350")
 
-        self.label = tk.Label(self)
+        self.label = tk.CTkLabel(self)
         self.label.place(x=35, y=15)
 
         self.countdown(60)
 
     def countdown(self, count):
-        self.label['text'] = count
+        self.label.configure(text=count)
 
         if count > 0:
             self.after(1000, self.countdown, count-1)
