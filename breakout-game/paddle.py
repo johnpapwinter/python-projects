9cbc6cf665059f3d7cd687398461f9fa1f4ac381
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2023-04-05 19:45:06 +0300

    added paddle

---
+++ breakout-game/paddle.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
+
+    def go_right(self):
+        new_x = self.xcor() + 20
+        self.goto(new_x, self.ycor())
+
+    def go_left(self):
+        new_x = self.xcor() - 20
+        self.goto(new_x, self.ycor())
+
