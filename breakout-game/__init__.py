1b6c2bca1f9bd180343e41ddcb32473a184465a2
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2023-04-06 22:13:03 +0300

    paddle movement and position

--- breakout-game/__init__.py
+++ breakout-game/__init__.py
@@ -1,4 +1,25 @@
+from turtle import Screen
+from paddle import Paddle
 
 
-if __name__ == '__main__':
-    pass
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("BREAKOUT")
+screen.tracer(0)
+
+player_paddle = Paddle((0, -280))
+
+screen.listen()
+screen.onkeypress(player_paddle.go_left, "Left")
+screen.onkeypress(player_paddle.go_right, "Right")
+
+game_in_on = True
+while game_in_on:
+    screen.update()
+
+
+screen.exitonclick()
+
+# if __name__ == '__main__':
+#     pass
--- breakout-game/paddle.py
+++ breakout-game/paddle.py
@@ -2,10 +2,12 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
+
     def __init__(self, position):
         super().__init__()
         self.shape("square")
-        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.shapesize(stretch_wid=1, stretch_len=5)
+        self.color("white")
         self.penup()
         self.goto(position)
 
