cdea062a2176063f279ed3e4b85aa46f755ff0a6
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2023-04-07 00:37:58 +0300

    paddle hits walls

--- breakout-game/paddle.py
+++ breakout-game/paddle.py
@@ -13,9 +13,13 @@ class Paddle(Turtle):
 
     def go_right(self):
         new_x = self.xcor() + 20
+        if self.xcor() > 320:
+            return
         self.goto(new_x, self.ycor())
 
     def go_left(self):
         new_x = self.xcor() - 20
+        if self.xcor() < -320:
+            return
         self.goto(new_x, self.ycor())
 
