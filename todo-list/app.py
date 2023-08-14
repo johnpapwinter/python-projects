7286c165624a361d7bb3fe9823c09e647365d77a
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2023-08-14 09:56:57 +0300

    model and db connection

--- todo-list/app.py
+++ todo-list/app.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from db import db
 
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+
+db.init_app(app)
---
+++ todo-list/model.py
@@ -0,0 +1,9 @@
+from db import db
+
+
+class ItemModel(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(500), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False)
