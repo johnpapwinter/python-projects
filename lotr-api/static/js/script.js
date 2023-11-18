e98ace2b14b9a4f82ad11c60ff75a6c00a7e641c
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2023-11-18 13:57:29 +0200

    api-based website frontend implementation

---
+++ lotr-api/static/js/script.js
@@ -0,0 +1,6 @@
+function clearForm() {
+    document.getElementById('name').value = '';
+    document.getElementById('gender').value = '';
+    document.getElementById('race').value = '';
+    document.getElementById('realm').value = '';
+}
